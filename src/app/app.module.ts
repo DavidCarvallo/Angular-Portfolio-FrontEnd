1b86b75eb573624dfa7ae79510bd3a5c9f7173c4
DavidCarvallo <carvallodavid78@gmail.com>
2022-09-29 11:18:52 -0300

    resubiendo proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { ExperienceComponent } from './componentes/experience/experience.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
